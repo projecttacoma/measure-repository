bb4fbae8c12306b4990aa6c2659016ddebb0941f
Elsa <eperelli@mitre.org>
2022-12-09 11:56:13 -0500

    Changed consts to functions in test util file and map to for...of

--- test/utils.ts
+++ test/utils.ts
@@ -2,22 +2,21 @@ import { Connection } from '../src/db/Connection';
 
 const dbUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/measure-repository-test';
 
-const createTestResource = async (data: any, resourceType: string) => {
+async function createTestResource(data: any, resourceType: string) {
   const collection = Connection.db.collection(resourceType);
   await collection.insertOne(data);
   return { id: data.id };
-};
+}
 
 export async function cleanUpDb() {
   await Connection.db.dropDatabase();
   await Connection.connection?.close();
 }
 
-export const testSetup = async (testFixtureList: fhir4.FhirResource[]) => {
+export async function testSetup(testFixtureList: fhir4.FhirResource[]) {
   await Connection.connect(dbUrl);
 
-  const result = testFixtureList.map(async x => {
-    return await createTestResource(x, x.resourceType);
-  });
-  await Promise.all(result);
-};
+  for (const resource of testFixtureList) {
+    await createTestResource(resource, resource.resourceType);
+  }
+}
