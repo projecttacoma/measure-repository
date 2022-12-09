3be357cede89c564fc8d969db261911a2b7b2e2f
Elsa <eperelli@mitre.org>
2022-12-09 12:21:23 -0500

    Updated import

--- src/services/MeasureService.ts
+++ src/services/MeasureService.ts
@@ -1,5 +1,4 @@
 import { loggers, RequestArgs } from '@projecttacoma/node-fhir-server-core';
-import { findResourceById } from '../db/dbOperations';
 import { Service } from '../types/service';
 import { ResourceNotFoundError } from '../util/errorUtils';
 
