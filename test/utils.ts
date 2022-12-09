e2d9a33a5672b5262d49d5f772706173b7cdcb2e
Elsa <eperelli@mitre.org>
2022-12-09 13:50:40 -0500

    Changed the types in createTestResource

--- test/utils.ts
+++ test/utils.ts
@@ -1,12 +1,13 @@
+import { FhirResourceType } from '@projecttacoma/node-fhir-server-core';
 import { Connection } from '../src/db/Connection';
 
 const dbUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/measure-repository-test';
 
-async function createTestResource(data: any, resourceType: string) {
-  const collection = Connection.db.collection(resourceType);
+const createTestResource = async (data: fhir4.FhirResource, resourceType: FhirResourceType) => {
+  const collection = Connection.db.collection<fhir4.FhirResource>(resourceType);
   await collection.insertOne(data);
   return { id: data.id };
-}
+};
 
 export async function cleanUpDb() {
   await Connection.db.dropDatabase();
