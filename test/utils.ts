8ffeea79c2b84b790e8bde0288bb1795d054003e
Elsa <eperelli@mitre.org>
2022-12-13 11:36:14 -0500

    async function for consistency

--- test/utils.ts
+++ test/utils.ts
@@ -1,10 +1,10 @@
 import { FhirResourceType } from '@projecttacoma/node-fhir-server-core';
 import { Connection } from '../src/db/Connection';
 
-const createTestResource = async (data: fhir4.FhirResource, resourceType: FhirResourceType) => {
+async function createTestResource(data: fhir4.FhirResource, resourceType: FhirResourceType) {
   const collection = Connection.db.collection<fhir4.FhirResource>(resourceType);
   await collection.insertOne({ ...data });
-};
+}
 
 export async function cleanUpDb() {
   await Connection.db.dropDatabase();
