197f9c19e0188b4edb2ab6b6c9f16577b7830bf4
Elsa <eperelli@mitre.org>
2023-03-20 14:53:32 -0400

    Prettier fixes

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,37 +1,29 @@
-import "@/styles/globals.css";
-import { AppShell, Center, Header, MantineProvider } from "@mantine/core";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import '@/styles/globals.css';
+import { AppShell, Center, Header, MantineProvider } from '@mantine/core';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Measure Repository Service Frontend</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          colorScheme: "light",
+          colorScheme: 'light'
         }}
       >
         <AppShell
           padding="md"
           header={
-            <Header
-              height={80}
-              style={{ backgroundColor: "#bdebf0", color: "#4a4f4f" }}
-            >
+            <Header height={80} style={{ backgroundColor: '#bdebf0', color: '#4a4f4f' }}>
               <Center>
-                <h1 style={{ marginTop: "12px", cursor: "pointer" }}>
-                  Measure Repository Service Frontend
-                </h1>
+                <h1 style={{ marginTop: '12px', cursor: 'pointer' }}>Measure Repository Service Frontend</h1>
               </Center>
             </Header>
           }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Center } from "@mantine/core";
+import { Center } from '@mantine/core';
 
 export default function Home() {
   return (
     <>
-      <Center style={{ alignItems: "center", paddingTop: "80px" }}>
+      <Center style={{ alignItems: 'center', paddingTop: '80px' }}>
         <div>Coming soon!</div>
       </Center>
     </>
