71c49be6d13cfd2b077dbdce2691402330c3783b
lmd59 <laurend@mitre.org>
2023-04-04 16:33:44 -0400

    Initial resource counts in the navbar

---
+++ frontend/components/ResourceCounts.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import { Badge, Button, Stack } from '@mantine/core';
+import Link from 'next/link';
+
+/**
+ * Component which retrieves all resources and their counts, calls on helper functions to sort them by count, then
+ * translates them into buttons
+ * @returns array of JSX Buttons
+ */
+const ResourceCounts = () => {
+  // set initial state to no resources
+  const [resources, setResources] = useState<Record<string, number>>({
+    Measure: 0,
+    Library: 0
+  });
+  useEffect(() => {
+    // Note: count may not update without refresh if there ends up being a way to change it on the server
+    Promise.all([
+      fetch(`${process.env.NEXT_PUBLIC_MRS_SERVER}/Measure`),
+      fetch(`${process.env.NEXT_PUBLIC_MRS_SERVER}/Library`)
+    ])
+      .then(([resMeasure, resLibrary]) => Promise.all([resMeasure.json(), resLibrary.json()]))
+      .then(([measureBundle, libraryBundle]) => {
+        setResources({ Measure: measureBundle.total || 0, Library: libraryBundle.total || 0 });
+      })
+      .catch(error => {
+        console.log(error.message, '...start the server');
+      });
+  }, []);
+
+  /**
+   * Returns the resource counts key:value pairs object as an array of buttons
+   * @returns array of JSX Buttons that are the sorted resources and their counts
+   */
+  const ResourceButtonsGroup = () => {
+    const buttonArray = Object.keys(resources).map(resourceType => (
+      <Link href={`/${resourceType}`} key={resourceType}>
+        <Button
+          fullWidth
+          compact
+          color="cyan"
+          radius="md"
+          size="md"
+          variant="subtle"
+          styles={{
+            inner: {
+              paddingLeft: '15px',
+              justifyContent: 'flex-start'
+            }
+          }}
+          rightIcon={<Badge color="cyan">{resources[resourceType]}</Badge>}
+          key={resourceType}
+        >
+          {resourceType}
+        </Button>
+      </Link>
+    ));
+    return <div> {buttonArray} </div>;
+  };
+
+  return (
+    <Stack align="flex-start" spacing="xs" style={{ marginBottom: 30 }}>
+      <ResourceButtonsGroup />
+    </Stack>
+  );
+};
+
+export { ResourceCounts };
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@/styles/globals.css';
-import { AppShell, Center, Header, MantineProvider } from '@mantine/core';
+import { AppShell, Center, Header, MantineProvider, Navbar, Text, Divider, Box, ScrollArea } from '@mantine/core';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ResourceCounts } from '../components/ResourceCounts';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -20,6 +21,30 @@ export default function App({ Component, pageProps }: AppProps) {
       >
         <AppShell
           padding="md"
+          navbar={
+            <Navbar width={{ base: '18vw' }} height="90vh" p="xs">
+              <Navbar.Section>
+                <Box
+                  sx={theme => ({
+                    backgroundColor: 'white',
+                    textAlign: 'center',
+                    paddingTop: '8px',
+                    paddingBottom: '12px',
+                    borderRadius: theme.radius.xs,
+                    cursor: 'pointer'
+                  })}
+                >
+                  <Text size="xl" weight={700} color="gray">
+                    Resources
+                  </Text>
+                </Box>
+              </Navbar.Section>
+              <Divider my="sm" style={{ paddingBottom: '15px' }} />
+              <Navbar.Section grow component={ScrollArea} mt="-xs" mb="-xs" ml="-xl" mr="-xs">
+                <ResourceCounts />
+              </Navbar.Section>
+            </Navbar>
+          }
           header={
             <Header height={80} style={{ backgroundColor: '#bdebf0', color: '#4a4f4f' }}>
               <Center>
