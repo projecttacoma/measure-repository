33efadaae4eec3811b1b350792905081fd1dd451
Elsa Perelli <eperelli@mitre.org>
2023-04-06 16:29:27 -0400

    Transaction bundle upload (#25)
    * Transaction bundle upload
    
    * Grammar fixes
    
    * Changed error message, fixed unit tests
    
    * Avoid app code duplication
    
    * Add/move bad request errors
    
    * Grammar fixes
    
    * Remove method param from insertBundleResources

---
+++ backend/src/services/BaseService.ts
@@ -0,0 +1,125 @@
+import { loggers, constants, resolveSchema } from '@projecttacoma/node-fhir-server-core';
+import { BadRequestError } from '../util/errorUtils';
+import { checkContentTypeHeader } from '../util/inputUtils';
+import { v4 as uuidv4 } from 'uuid';
+import { createResource, updateResource } from '../db/dbOperations';
+import path from 'path';
+import { DetailedEntry, replaceReferences } from '../util/baseUtils';
+
+const logger = loggers.get('default');
+
+/**
+ * Uploads a transaction bundle to the server and returns the transaction-response bundle.
+ */
+export async function uploadTransactionBundle(req: any, res: any) {
+  logger.info('POST /');
+  const contentType: string | undefined = req.headers['content-type'];
+  checkContentTypeHeader(contentType);
+
+  const entries: DetailedEntry[] = req.body.entry;
+  const baseVersion: keyof typeof constants.VERSIONS = req.params.base_version;
+
+  if (req.body.resourceType !== 'Bundle') {
+    throw new BadRequestError(`Expected 'resourceType: Bundle'. Received 'resourceType: ${req.body.resourceType}'.`);
+  }
+
+  if (req.body.type.toLowerCase() !== 'transaction') {
+    throw new BadRequestError(`Expected 'type: transaction'. Received 'type: ${req.body.type}'.`);
+  }
+
+  const requestResults = await uploadResourcesFromBundle(entries);
+
+  const bundle = makeTransactionResponseBundle(requestResults, res, baseVersion);
+  logger.info('Transaction bundle successfully uploaded.');
+  return bundle;
+}
+
+/**
+ * Uploads Library and Measure resources from a transaction bundle to the server.
+ */
+async function uploadResourcesFromBundle(entries: DetailedEntry[]) {
+  logger.info('Inserting Measure and Library resources from transaction bundle');
+  const scrubbedEntries = replaceReferences(entries);
+
+  const requestsArray = scrubbedEntries.map(async entry => {
+    if (entry.request) {
+      const { method } = entry.request;
+      if (method !== 'PUT' && method !== 'POST') {
+        throw new BadRequestError(
+          `Expected requests of type PUT or POST, received ${method} for ${entry.resource?.resourceType}/${entry.resource?.id}`
+        );
+      } else {
+        return insertBundleResources(entry);
+      }
+    } else {
+      throw new BadRequestError('Each entry must contain request details that provide the HTTP details of the action.');
+    }
+  });
+  return Promise.all(requestsArray);
+}
+
+/**
+ * Inserts Library or Measure resources from Bundle into the database through create or update.
+ */
+async function insertBundleResources(entry: DetailedEntry) {
+  if (entry.resource?.resourceType === 'Library' || entry.resource?.resourceType === 'Measure') {
+    if (entry.isPost) {
+      entry.resource.id = uuidv4();
+      const { id } = await createResource(entry.resource, entry.resource.resourceType);
+      if (id != null) {
+        entry.status = 201;
+        entry.statusText = 'Created';
+      }
+    } else {
+      if (entry.resource.id) {
+        const { id, created } = await updateResource(entry.resource.id, entry.resource, entry.resource.resourceType);
+        if (created === true) {
+          entry.status = 201;
+          entry.statusText = 'Created';
+        } else if (id != null && created === false) {
+          entry.status = 200;
+          entry.statusText = 'OK';
+        }
+      } else {
+        throw new BadRequestError('Requests of type PUT must have an id associated with the resource.');
+      }
+    }
+  } else {
+    throw new BadRequestError(
+      entry.resource
+        ? `All resource entries must be of either resourceType 'Measure' or 'Library'. Received resourceType ${entry.resource?.resourceType}.`
+        : `All entries must contain resources of resourceType 'Measure' or 'Library'.`
+    );
+  }
+  return entry;
+}
+
+/**
+ * Creates transaction-response bundle.
+ */
+function makeTransactionResponseBundle(
+  requestResults: DetailedEntry[],
+  res: any,
+  baseVersion: keyof typeof constants.VERSIONS
+) {
+  logger.info('Compiling transaction-response bundle');
+  const Bundle = resolveSchema(baseVersion, 'bundle');
+  const responseBundle = new Bundle({ type: 'transaction-response', id: uuidv4() });
+  responseBundle.link = {
+    url: `${res.req.protocol}://${path.join(res.req.get('host'), res.req.baseUrl)}`,
+    relation: 'self'
+  };
+
+  const entries: fhir4.BundleEntry[] = [];
+  requestResults.forEach(result => {
+    const entry = new Bundle({ response: { status: `${result.status} ${result.statusText}` } });
+    if (result.status === 200 || result.status === 201) {
+      entry.response.location = `${baseVersion}/${result.resource?.resourceType}/${result.resource?.id}`;
+    }
+    entries.push(entry);
+  });
+
+  responseBundle.entry = entries;
+  logger.info('Completed transaction response');
+  return responseBundle;
+}
---
+++ backend/test/services/BaseService.test.ts
@@ -0,0 +1,255 @@
+import { initialize, Server } from '@projecttacoma/node-fhir-server-core';
+import supertest from 'supertest';
+import { app } from '../../src/app';
+import { serverConfig } from '../../src/config/serverConfig';
+import { cleanUpTestDatabase, setupTestDatabase } from '../utils';
+
+let server: Server;
+
+const INVALID_METHOD_REQ = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      resource: {
+        resourceType: 'Measure',
+        id: 'test-measure',
+        library: ['Library/test-library']
+      },
+      request: {
+        method: 'GET',
+        url: 'Measure/test-measure'
+      }
+    }
+  ]
+};
+
+const INVALID_PUT_REQ = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      resource: {
+        resourceType: 'Measure',
+        library: ['Library/test-library']
+      },
+      request: {
+        method: 'PUT',
+        url: 'Measure/test-measure'
+      }
+    }
+  ]
+};
+
+const INVALID_RESOURCE_REQ = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      resource: {
+        resourceType: 'Invalid',
+        id: 'test-library'
+      },
+      request: {
+        method: 'POST',
+        url: 'Library/test-library'
+      }
+    }
+  ]
+};
+
+const INVALID_ENTRY_REQ = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      invalid: {
+        resourceType: 'Invalid',
+        id: 'test-library'
+      },
+      request: {
+        method: 'POST',
+        url: 'Library/test-library'
+      }
+    }
+  ]
+};
+
+const INVALID_REQ = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      invalid: {
+        resourceType: 'Invalid',
+        id: 'test-library'
+      }
+    }
+  ]
+};
+
+const VALID_PUT_REQ = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      resource: {
+        resourceType: 'Measure',
+        id: 'test-measure',
+        library: ['Library/test-library']
+      },
+      request: {
+        method: 'PUT',
+        url: 'Measure/test-measure'
+      }
+    }
+  ]
+};
+
+const VALID_POST_REQ = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      resource: {
+        resourceType: 'Library',
+        id: 'test-library'
+      },
+      request: {
+        method: 'POST',
+        url: 'Library/test-library'
+      }
+    }
+  ]
+};
+
+describe('BaseService', () => {
+  beforeAll(async () => {
+    server = initialize(serverConfig, app);
+    return setupTestDatabase([]);
+  });
+
+  describe('uploadTransactionBundle', () => {
+    it('returns 200 and transaction-response bundle when a transaction bundle with correct resource, headers, and PUT request is uploaded', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send(VALID_PUT_REQ)
+        .set('content-type', 'application/json+fhir')
+        .expect(200)
+        .then(response => {
+          expect(response.body.resourceType).toEqual('Bundle');
+          expect(response.body.type).toEqual('transaction-response');
+          expect(response.body.entry[0].response.status).toEqual('201 Created');
+          expect(response.body.entry[0].response.location).toEqual('4_0_1/Measure/test-measure');
+        });
+    });
+
+    it('returns 200 and transaction-response bundle when a transaction bundle with correct resource, headers, and POST request is uploaded', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send(VALID_POST_REQ)
+        .set('content-type', 'application/json+fhir')
+        .expect(200)
+        .then(response => {
+          expect(response.body.resourceType).toEqual('Bundle');
+          expect(response.body.type).toEqual('transaction-response');
+          expect(response.body.entry[0].response.status).toEqual('201 Created');
+          expect(response.body.entry[0].response.location).toBeDefined();
+        });
+    });
+
+    it('returns 400 when the transaction bundle contains a resource that is not a Library or Measure', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send(INVALID_RESOURCE_REQ)
+        .set('content-type', 'application/json+fhir')
+        .expect(400)
+        .then(response => {
+          expect(response.body.issue[0].details.text).toEqual(
+            `All resource entries must be of either resourceType 'Measure' or 'Library'. Received resourceType Invalid.`
+          );
+        });
+    });
+
+    it('returns 400 when the transaction bundle contains an entry where entry.resource is undefined', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send(INVALID_ENTRY_REQ)
+        .set('content-type', 'application/json+fhir')
+        .expect(400)
+        .then(response => {
+          expect(response.body.issue[0].details.text).toEqual(
+            `All entries must contain resources of resourceType 'Measure' or 'Library'.`
+          );
+        });
+    });
+
+    it('returns 400 when the transaction bundle contains an entry where entry.request is undefined', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send(INVALID_REQ)
+        .set('content-type', 'application/json+fhir')
+        .expect(400)
+        .then(response => {
+          expect(response.body.issue[0].details.text).toEqual(
+            'Each entry must contain request details that provide the HTTP details of the action.'
+          );
+        });
+    });
+
+    it('returns 400 when resource type is not Bundle', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send({ resourceType: 'invalid', type: 'transaction' })
+        .set('content-type', 'application/json+fhir')
+        .expect(400)
+        .then(response => {
+          expect(response.body.issue[0].details.text).toEqual(
+            `Expected 'resourceType: Bundle'. Received 'resourceType: invalid'.`
+          );
+        });
+    });
+
+    it('returns 400 when Bundle is not of type transaction', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send({ resourceType: 'Bundle', type: 'invalid' })
+        .set('content-type', 'application/json+fhir')
+        .expect(400)
+        .then(response => {
+          expect(response.body.issue[0].details.text).toEqual(
+            `Expected 'type: transaction'. Received 'type: invalid'.`
+          );
+        });
+    });
+
+    it('returns 400 when transaction bundle has requests not of type PUT or POST', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send(INVALID_METHOD_REQ)
+        .set('content-type', 'application/json+fhir')
+        .expect(400)
+        .then(response => {
+          expect(response.body.issue[0].details.text).toEqual(
+            `Expected requests of type PUT or POST, received GET for Measure/test-measure`
+          );
+        });
+    });
+
+    it('returns 400 when transaction bundle has a PUT request that does not contain an id for the resource', async () => {
+      await supertest(server.app)
+        .post('/4_0_1/')
+        .send(INVALID_PUT_REQ)
+        .set('content-type', 'application/json+fhir')
+        .expect(400)
+        .then(response => {
+          expect(response.body.issue[0].details.text).toEqual(
+            `Requests of type PUT must have an id associated with the resource.`
+          );
+        });
+    });
+  });
+  afterAll(() => {
+    return cleanUpTestDatabase();
+  });
+});
