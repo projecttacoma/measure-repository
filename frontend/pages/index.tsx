d20ff74fcbee352bd5aafdf70965f0bf46f39343
lmd59 <laurend@mitre.org>
2023-05-10 10:33:55 -0400

    Show resources, operations, and search parameters that are part of th… (#33)
    * Show resources, operations, and search parameters that are part of the capability statement
    
    * Apply suggestions from code review
    
    PR comments
    
    Co-authored-by: natjoe4 <35576465+natjoe4@users.noreply.github.com>
    
    * alphabetize operations and search parameters
    
    * alphabetize resource list
    
    ---------
    
    Co-authored-by: natjoe4 <35576465+natjoe4@users.noreply.github.com>

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Anchor, Divider, List, Text, Title } from '@mantine/core';
+import { Anchor, Divider, List, Table, Text, Title } from '@mantine/core';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+export default function Home({ capabilityStatement }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div style={{ display: 'flex', height: '100%', width: '100%', flexDirection: 'column', padding: '4px 20px' }}>
       <Title order={2} underline>
@@ -25,8 +26,35 @@ export default function Home() {
       <Text>More functionality coming soon!</Text>
       <Divider my="sm" variant="dotted" />
       <Title order={2} underline style={{ marginBottom: '8px' }}>
-        Capabilities
+        Capabilities:
       </Title>
+      <Title order={3} underline>
+        Capability Statement
+      </Title>
+      <div style={{ marginBottom: '8px' }}>
+        {capabilityStatement ? (
+          <Table>
+            <thead>
+              <tr>
+                <th>Resource</th>
+                <th>Operations</th>
+                <th>Search Parameters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {capabilityStatement.rest?.[0]?.resource
+                ?.sort((a, b) => {
+                  return a.type > b.type ? 1 : -1; // sort alphabetical by resource type
+                })
+                .map(r => CapabilityRow(r))}
+            </tbody>
+          </Table>
+        ) : (
+          <Text c="red.7" style={{ marginBottom: '8px' }}>
+            Capability Statement Unavailable
+          </Text>
+        )}
+      </div>
       <Title order={3} underline>
         Resource Read and Search
       </Title>
@@ -40,7 +68,7 @@ export default function Home() {
         parameters on the search page.
       </Text>
       <Title order={3} underline>
-        Authoring Functionality:
+        Authoring Functionality
       </Title>
       <Text>
         This application will provide a FHIR Measure and Library authoring environment as outlined in the{' '}
@@ -85,3 +113,38 @@ export default function Home() {
     </div>
   );
 }
+
+export function CapabilityRow(resource: fhir4.CapabilityStatementRestResource) {
+  return (
+    <tr key={`row-${resource.type}`}>
+      <td>{resource.type}</td>
+      <td>
+        {resource.operation
+          ?.map(o => {
+            return o.name;
+          })
+          .sort()
+          .join(', ')}
+      </td>
+      <td>
+        {resource.searchParam
+          ?.map(sp => {
+            return sp.name;
+          })
+          .sort()
+          .join(', ')}
+      </td>
+    </tr>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  capabilityStatement: fhir4.CapabilityStatement | null;
+}> = async () => {
+  // Fetch CapabilityStatement
+  const res = await fetch(`${process.env.NEXT_PUBLIC_MRS_SERVER}/metadata`);
+  const capabilityStatement = res.status === 200 ? ((await res.json()) as fhir4.CapabilityStatement) : null;
+
+  // Pass to the page via props
+  return { props: { capabilityStatement } };
+};
