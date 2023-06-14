19cc81a2dcf3bfa7b20993609def9fc933423def
Elsa <eperelli@mitre.org>
2023-06-14 15:58:08 -0400

    Split up optional inputs

--- app/src/server/trpc/routers/draft.ts
+++ app/src/server/trpc/routers/draft.ts
@@ -20,8 +20,10 @@ export const draftRouter = router({
     .query(async opts => getAllDraftsByType<FhirArtifact>(opts.input)),
 
   getDraftById: publicProcedure
-    .input(z.object({ id: z.string(), resourceType: z.enum(['Measure', 'Library']) }).optional())
-    .query(async ({ input }) => (input ? getDraftById<FhirArtifact>(input.id, input.resourceType) : null)),
+    .input(z.object({ id: z.string().optional(), resourceType: z.enum(['Measure', 'Library']).optional() }))
+    .query(async ({ input }) =>
+      input.id && input.resourceType ? getDraftById<FhirArtifact>(input.id, input.resourceType) : null
+    ),
 
   createDraft: publicProcedure
     .input(z.object({ resourceType: z.enum(['Measure', 'Library']), draft: z.any() }))
