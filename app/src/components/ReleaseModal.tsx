5e5f65f7782005013555f60c3c662478fd9de363
BriannaVH <vanheckeb@merrimack.edu>
2023-08-07 14:15:45 -0400

    Show artifact details in modal

--- app/src/components/ReleaseModal.tsx
+++ app/src/components/ReleaseModal.tsx
@@ -1,9 +1,26 @@
 import { trpc } from '@/util/trpc';
 import { ArtifactResourceType } from '@/util/types/fhir';
-import { Modal, Button, Center, Group, Text, Grid, TextInput } from '@mantine/core';
+import {
+  Anchor,
+  Avatar,
+  Blockquote,
+  Button,
+  Center,
+  Divider,
+  Group,
+  HoverCard,
+  List,
+  Modal,
+  Stack,
+  Space,
+  Text,
+  TextInput
+} from '@mantine/core';
 import { DateTime } from 'luxon';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import DraftListItem from './DraftListItem';
+import { Flame, InfoCircle } from 'tabler-icons-react';
 
 export interface ReleaseModalProps {
   open: boolean;
@@ -71,37 +88,94 @@ export default function ReleaseModal({ open = true, onClose, id, resourceType }:
     onClose();
   }
   return (
-    <Modal
-      opened={open}
-      onClose={onClose}
-      withCloseButton={false}
-      title={`Release ${resourceType} ${id}`}
-      size="lg"
-      centered
-    >
+    <Modal opened={open} onClose={onClose} withCloseButton={false} size="65%" centered>
+      <Stack>
+        <div>
+          <Group spacing="lg">
+            <Text size="xl" fw={700}>
+              {resource?.name
+                ? `Release ${resourceType} ${resource?.name}?`
+                : `Release ${resourceType}/${resource?.id}?`}
+            </Text>
+            <HoverCard width={620} shadow="md" withArrow openDelay={200} closeDelay={200}>
+              <HoverCard.Target>
+                <div>
+                  <InfoCircle size="1.5rem" style={{ opacity: 0.5 }} />
+                </div>
+              </HoverCard.Target>
+              <HoverCard.Dropdown>
+                <Group>
+                  <Avatar color="red" radius="xl">
+                    <Flame size="1.5rem" />
+                  </Avatar>
+                  <Stack spacing={5}>
+                    <Text size="sm" weight={700} sx={{ lineHeight: 1 }}>
+                      Learn More
+                    </Text>
+                    <Anchor
+                      target="_blank"
+                      href="https://build.fhir.org/ig/HL7/cqf-measures/measure-repository-service.html#release"
+                      color="dimmed"
+                      size="xs"
+                      sx={{ lineHeight: 1 }}
+                    >
+                      Release Draft Artifacts
+                    </Anchor>
+                  </Stack>
+                </Group>
+                <Space h="lg" />
+                <Blockquote cite="– Fhir Spec" sx={{ lineHeight: 2, fontSize: '15px' }}>
+                  The release operation supports updating the status of an existing draft artifact to active. The
+                  operation sets the date and status elements of the artifact, but is otherwise not allowed to change
+                  any other elements of the artifact. Child artifacts (i.e. artifacts that compose the existing
+                  artifact) are also released, recursively. To be released, an artifact is required to have a version
+                  specified.
+                </Blockquote>
+              </HoverCard.Dropdown>
+            </HoverCard>
+          </Group>
+        </div>
+      </Stack>
+      <Space h="md" />
+      <Divider size="sm" />
+      <Space h="lg" />
+      <List style={{ listStyle: 'none' }}>
+        {resource?.id && <DraftListItem header={'ID'} context={resource.id} originalColor={'#F8F9FA'} />}
+        {resource?.url && <DraftListItem header={'URL'} context={resource.url} originalColor={'#F1F3F5'} />}
+        {resource?.name && <DraftListItem header={'Name'} context={resource.name} originalColor={'#F8F9FA'} />}
+        {resource?.resourceType && (
+          <DraftListItem header={'Type'} context={resource.resourceType} originalColor={'#F1F3F5'} />
+        )}
+        {resource?.description && (
+          <DraftListItem header={'Description'} context={resource.description} originalColor={'#F8F9FA'} />
+        )}
+        {resource?.version && <DraftListItem header={'Version'} context={resource.version} originalColor={'#F1F3F5'} />}
+        {resource?.effectivePeriod && (
+          <DraftListItem header={'Effective Period'} date={resource.effectivePeriod} originalColor={'#F1F3F5'} />
+        )}
+      </List>
+      <Space h="lg" />
       <Center>
-        {/* TODO: add more text reflecting draft details */}
-        <Grid>
-          <Grid.Col span={10}>
-            <div>
-              <Text size="lg" fw={700}>
-                {resource?.name ?? `${resourceType}/${resource?.id}`}
-              </Text>
-            </div>
-            {resource?.url && <Text size="sm">{resource.url}</Text>}
-            <TextInput label="Add version" value={version} onChange={e => setVersion(e.target.value)} />
-          </Grid.Col>
-        </Grid>
-      </Center>
-      <Center>
-        <Group pt={8} position="right">
-          <Button onClick={confirm} disabled={!version}>
-            Release
-          </Button>
-          <Button variant="default" onClick={onClose}>
-            Cancel
-          </Button>
-        </Group>
+        <Stack>
+          <TextInput
+            size="md"
+            style={{ width: '55rem' }}
+            radius="md"
+            withAsterisk
+            label="Add version"
+            value={version}
+            placeholder="1.0.0"
+            onChange={e => setVersion(e.target.value)}
+          />
+          <Group pt={8} position="center">
+            <Button onClick={confirm} disabled={!version}>
+              Release
+            </Button>
+            <Button variant="default" onClick={onClose}>
+              Cancel
+            </Button>
+          </Group>
+        </Stack>
       </Center>
     </Modal>
   );
