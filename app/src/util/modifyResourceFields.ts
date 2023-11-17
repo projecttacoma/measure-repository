6e0d6e3e55c4cee18bf27b675b4fb41f30ff367b
Elsa Perelli <eperelli@mitre.org>
2023-11-17 11:39:05 -0500

    Add versions to draft artifacts (#67)
    * Force semantic versioning on artifact uploads
    
    * Remove version input in ReleaseModal, increment version when converting artifact to draft
    
    * Add unit tests, remove change to typing
    
    * Remove coercing of semantic versioning from backend, increment version in x.x.x or x.x.xxx format
    
    * Remove versioning section from README
    
    * Remove console.log, add comments
    
    * Stricter typing to FhirArtifacts

--- app/src/util/modifyResourceFields.ts
+++ app/src/util/modifyResourceFields.ts
@@ -1,13 +1,73 @@
+import { coerce, inc } from 'semver';
 import { FhirArtifact } from './types/fhir';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
  * Helper function that takes an artifact and returns it with a new id,
- * draft status, and no version
+ * draft status, and increments the version if it has one or sets it to
+ * 0.0.1 if it does not
  */
 export function modifyResourceToDraft(artifact: FhirArtifact) {
   artifact.id = uuidv4();
   artifact.status = 'draft';
-  delete artifact.version;
+  // we can only increment artifacts whose versions are either semantic, can be coerced
+  // to semantic, or are in x.x.xxx format. Every other kind of version will become 0.0.1
+  if (artifact.version) {
+    const coerced = coerce(artifact.version);
+    const incVersion = coerced !== null ? inc(coerced, 'patch') : null;
+    if (incVersion !== null) {
+      artifact.version = incVersion;
+      // if we can't coerce the version to semver, then check that it is x.x.xxx
+      // format and increment manually
+    } else if (checkVersionFormat(artifact.version)) {
+      artifact.version = incrementArtifactVersion(artifact.version);
+    } else {
+      // if it cannot be coerced and is not x.x.xxx format, then set to 0.0.1
+      artifact.version = '0.0.1';
+    }
+  }
   return { id: artifact.id, ...artifact };
+}
+
+/**
+ * Increments an artifact version that is in x.x.xxx format
+ */
+function incrementArtifactVersion(version: string): string {
+  const versionParts = version.split('.').map(Number);
+
+  // increment the patch version
+  versionParts[2]++;
+
+  // if the patch version reaches 1000, reset it to 0 and increment the minor version
+  if (versionParts[2] >= 1000) {
+    versionParts[2] = 0;
+    versionParts[1]++;
+  }
+
+  // if the minor version reaches 10, reset it to 0 and increment the major version
+  if (versionParts[1] >= 10) {
+    versionParts[1] = 0;
+    versionParts[0]++;
+  }
+
+  let formattedPatch;
+  if (versionParts[2] < 10) {
+    formattedPatch = versionParts[2].toString().padStart(3, '0');
+  } else if (versionParts[2] < 100) {
+    formattedPatch = versionParts[2].toString().padStart(2, '0');
+  } else {
+    formattedPatch = versionParts[2].toString();
+  }
+
+  return `${versionParts[0]}.${versionParts[1]}.${formattedPatch}`;
+}
+
+/**
+ * Takes in an artifact version and returns true if it is in x.x.xxx
+ * format and returns false if it is not
+ */
+function checkVersionFormat(version: string): boolean {
+  const format = /^\d\.\d\.\d{3}$/;
+
+  return format.test(version);
 }
